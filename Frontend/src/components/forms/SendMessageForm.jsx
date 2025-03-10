0c5d227de7d7dea39212bdabb7544d3726ca46ad
matej2314 <mateo2314@gmail.com>
2025-03-10 17:08:13 +0100

    Modified websocket server config and Frontend url variables

--- Frontend/src/components/forms/SendMessageForm.jsx
+++ Frontend/src/components/forms/SendMessageForm.jsx
@@ -1,102 +1,77 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 import { useMessagesStore } from '../../store/messagesStore';
 import sendRequest from '../../utils/asyncUtils/sendRequest';
 import { serverUrl } from '../../url';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 import { showInfoToast, showErrorToast } from '../../configs/toastify';
-import SendMessageBtn from "./internal/SendMessageBtn";
+import SendMessageBtn from './internal/SendMessageBtn';
 import LoadingModal from '../modals/LoadingModal';
 
 export default function SendMessageForm({ reply, recipientName, onClose }) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [actionState, setActionState] = useState({ type: null });
-    const { fetchMessages } = useMessagesStore();
-    const recipientRef = useRef();
-    const messageContentRef = useRef();
+	const [isLoading, setIsLoading] = useState(false);
+	const [actionState, setActionState] = useState({ type: null });
+	const { fetchMessages } = useMessagesStore();
+	const recipientRef = useRef();
+	const messageContentRef = useRef();
 
-    const handleSetActionState = (action) => {
-        setActionState({ type: action });
-    };
+	const handleSetActionState = (action) => {
+		setActionState({ type: action });
+	};
 
-    const handleResetActionState = () => {
-        setActionState({ type: null });
-    }
+	const handleResetActionState = () => {
+		setActionState({ type: null });
+	};
 
-    const handleSendMessage = async (e) => {
-        e.preventDefault();
+	const handleSendMessage = async (e) => {
+		e.preventDefault();
 
-        const messageData = {
-            recipientName: recipientRef.current.value,
-            content: messageContentRef.current.value,
-        };
+		const messageData = {
+			recipientName: recipientRef.current.value,
+			content: messageContentRef.current.value,
+		};
 
-        try {
-            setIsLoading(true);
-            const sendMessage = await sendRequest('POST', messageData, `${serverUrl}/message/send`);
+		try {
+			setIsLoading(true);
+			const sendMessage = await sendRequest('POST', messageData, `${serverUrl}/message/send`);
 
-            if (sendMessage.status === 'success') {
-                showInfoToast(sendMessage.message);
-                await fetchMessages();
-                messageContentRef.current.value = "";
-                setTimeout(() => {
-                    onClose();
-                }, 600);
-            }
-        } catch (error) {
-            showErrorToast(sendMessage.message);
-        } finally {
-            handleSetActionState('sended');
-            setIsLoading(false);
-        }
-    };
+			if (sendMessage.status === 'success') {
+				showInfoToast(sendMessage.message);
+				await fetchMessages();
+				messageContentRef.current.value = '';
+				setTimeout(() => {
+					onClose();
+				}, 600);
+			}
+		} catch (error) {
+			showErrorToast('Failed to send message.');
+		} finally {
+			handleSetActionState('sended');
+			setIsLoading(false);
+		}
+	};
 
-    return (
-        <div className="w-full h-full flex flex-col justify-center items-center gap-4">
-            <h2 className="text-2xl mb-2">
-                {reply ? 'Reply message' : 'Send message'}
-            </h2>
-            <form
-                onSubmit={handleSendMessage}
-                className="w-full h-full flex flex-col items-center justify-center gap-3"
-            >
-                <label className="w-full h-fit flex justify-center" htmlFor="recipientName">Type recipient name:</label>
-                <div className="relative w-fit flex justify-center">
-                    <input
-                        type="text"
-                        name="recipientName"
-                        id="recipientName"
-                        placeholder="recipient name"
-                        ref={recipientRef}
-                        defaultValue={recipientName || ""}
-                        required
-                        disabled={recipientName}
-                        onInput={(e) => e.target.nextSibling.style.display = e.target.value ? 'none' : 'block'}
-                        className={`${recipientName ? "bg-gray-300 cursor-not-allowed" : ""} input-base`}
-                    />
-                    <Icon
-                        icon='mage:user-fill'
-                        className="icon-base top-0.5 text-gray-500 text-xl text-opacity-40"
-                    />
-                </div>
-                <label className="w-full h-fit flex justify-center" htmlFor="messageContent">Type your message:</label>
-                <div className="relative w-full flex justify-center">
-                    <textarea
-                        className=" md:w-1/2 resize-none input-base"
-                        name="messageContent"
-                        id="messageContent"
-                        placeholder="message"
-                        ref={messageContentRef}
-                        required
-                    />
-                    <Icon
-                        icon='ic:baseline-alternate-email'
-                        className="absolute right-[6.5rem] pointer-events-none top-0.5 text-slate-600 text-xl text-opacity-45"
-                    />
-                </div>
-                <SendMessageBtn form='sendMessage' state={actionState} setState={handleSetActionState} resetState={handleResetActionState} />
-            </form>
-            {isLoading && <LoadingModal isOpen={isLoading} />}
-        </div>
-    );
+	return (
+		<div className="w-8/12 xl:w-10/12 h-full flex flex-col justify-center items-center gap-4">
+			<h2 className="text-xl xl:text-2xl mb-2">{reply ? 'Reply message' : 'Send message'}</h2>
+			<form onSubmit={handleSendMessage} className="w-full h-full flex flex-col items-center justify-center gap-3">
+				<label className="w-full h-fit flex justify-center" htmlFor="recipientName">
+					Type recipient name:
+				</label>
+				<div className="relative w-fit flex justify-center">
+					<input type="text" name="recipientName" id="recipientName" placeholder="recipient name" ref={recipientRef} defaultValue={recipientName || ''} required disabled={recipientName} onInput={(e) => (e.target.nextSibling.style.display = e.target.value ? 'none' : 'block')} className={`${recipientName ? 'bg-gray-300 cursor-not-allowed' : ''} input-base`} />
+					<Icon icon="mage:user-fill" className="icon-base top-0.5 text-gray-500 text-xl text-opacity-40" />
+				</div>
+				<label className="w-full h-fit flex justify-center" htmlFor="messageContent">
+					Type your message:
+				</label>
+				<div className="relative w-[13rem] indirect:w-full indirectxl:w-10/12  sm:w-9/12 md:w-8/12 lg:w-6/12 xl:w-11/12 flex justify-center">
+					<textarea className=" resize-none input-base" name="messageContent" id="messageContent" placeholder="message" ref={messageContentRef} required />
+					<Icon icon="ic:outline-message" className="absolute right-1 pointer-events-none top-0.5 text-slate-600 text-xl text-opacity-45" />
+				</div>
+				<SendMessageBtn form="sendMessage" state={actionState} setState={handleSetActionState} resetState={handleResetActionState} />
+			</form>
+			{isLoading && <LoadingModal isOpen={isLoading} />}
+		</div>
+	);
 }
--- Frontend/src/components/modals/messagesModals/messagesModals.jsx
+++ Frontend/src/components/modals/messagesModals/messagesModals.jsx
@@ -4,163 +4,109 @@ import { useMessagesStore } from '../../../store/messagesStore';
 import Modal from 'react-modal';
 import SendMessageForm from '../../forms/SendMessageForm';
 import { serverUrl } from '../../../url';
-import sendRequest from "../../../utils/asyncUtils/sendRequest";
+import sendRequest from '../../../utils/asyncUtils/sendRequest';
 import { showInfoToast, showErrorToast } from '../../../configs/toastify';
 import { Icon } from '@iconify/react';
 
 export function SendMessageModal({ isOpen, onRequestClose, recipient }) {
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            className='bg-slate-200 rounded-lg p-6 w-10/12 xl:w-1/3 mx-auto mt-20 shadow-lg border-4 border-slate-400'
-            overlayClassName='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
-        >
-            <div className='w-full flex justify-end'>
-                <button onClick={onRequestClose}
-                    className='relative left-3 bottom-6 text-black hover:text-gray-600'
-                >X</button>
-            </div>
-
-            <SendMessageForm reply={false} recipientName={recipient} onClose={onRequestClose} />
-        </Modal>
-    )
+	return (
+		<Modal isOpen={isOpen} onRequestClose={onRequestClose} className="bg-slate-200 rounded-lg p-6 w-10/12 xl:w-1/3 mx-auto mt-20 shadow-lg border-4 border-slate-400" overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+			<div className="w-full flex justify-end">
+				<button onClick={onRequestClose} className="relative left-3 bottom-6 text-black hover:text-gray-600">
+					X
+				</button>
+			</div>
+			<div className="w-full flex justify-center">
+				<SendMessageForm reply={false} recipientName={recipient} onClose={onRequestClose} />
+			</div>
+		</Modal>
+	);
 }
 
 export function DeleteMessageModal({ isOpen, onRequestClose, message }) {
-    const { fetchMessages } = useMessagesStore();
+	const { fetchMessages } = useMessagesStore();
 
+	const handleDeleteMessage = async (messageId) => {
+		const delData = {
+			messageId: messageId,
+		};
+		const result = await sendRequest('DELETE', delData, `${serverUrl}/message/delete`);
 
-    const handleDeleteMessage = async (messageId) => {
-        const delData = {
-            messageId: messageId,
-        };
-        const result = await sendRequest('DELETE', delData, `${serverUrl}/message/delete`)
+		if (result.status === 'error') {
+			showErrorToast('Failed to deleting message.');
+		} else if (result.status === 'success') {
+			showInfoToast('Message deleted correctly.');
+			onRequestClose();
+		}
+	};
 
-        if (result.status === 'error') {
-            showErrorToast('Failed to deleting message.');
-        } else if (result.status === 'success') {
-            showInfoToast('Message deleted correctly.');
-            onRequestClose();
-        }
-    };
-
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            className='del-msg-modal'
-            overlayClassName='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
-        >
-            <div className='w-full h-fit flex flex-col justify-center items-center gap-5'>
-                <p>{`Are you sure you want to delete the message '${message.message}' ?`}</p>
-                <div id='btnsDiv' className='w-full h-fit flex justify-around'>
-                    <button
-                        type="button"
-                        className="form-submit-modal-btn"
-                        onClick={() => handleDeleteMessage(message.id)}
-                    >
-                        Yes
-                    </button>
-                    <button
-                        onClick={onRequestClose}
-                        type="button"
-                        className="form-submit-modal-btn"
-                    >
-                        No
-                    </button>
-                </div>
-            </div>
-        </Modal>
-    )
+	return (
+		<Modal isOpen={isOpen} onRequestClose={onRequestClose} className="del-msg-modal" overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+			<div className="w-full h-fit flex flex-col justify-center items-center gap-5">
+				<p>{`Are you sure you want to delete the message '${message.message}' ?`}</p>
+				<div id="btnsDiv" className="w-full h-fit flex justify-around">
+					<button type="button" className="form-submit-modal-btn" onClick={() => handleDeleteMessage(message.id)}>
+						Yes
+					</button>
+					<button onClick={onRequestClose} type="button" className="form-submit-modal-btn">
+						No
+					</button>
+				</div>
+			</div>
+		</Modal>
+	);
 }
 export function DisplayMessageDetails({ isOpen, onRequestClose, message }) {
-    const { user } = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            className='display-msg-modal'
-            overlayClassName='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
-        >
-            <div className='relative w-full flex justify-end'>
-                <button onClick={onRequestClose}
-                    className='absolute -translate-y-6 translate-x-4 text-black hover:text-gray-600'
-                >X</button>
-            </div>
+	return (
+		<Modal isOpen={isOpen} onRequestClose={onRequestClose} className="display-msg-modal" overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+			<div className="relative w-full flex justify-end">
+				<button onClick={onRequestClose} className="absolute -translate-y-6 translate-x-4 text-black hover:text-gray-600">
+					X
+				</button>
+			</div>
+			<div className="w-full h-fit flex flex-col items-center gap-3 border-b-2 border-slate-400/20 pb-2 xl:pb-3">
+				<h2 className="text-xl xl:text-2xl">Details:</h2>
+				<form className="w-full h-fit flex flex-col items-center gap-2 xl:gap-3 text-sm xl:text-base">
+					<label className="w-full h-fit flex justify-center" htmlFor="senderName">
+						From:
+					</label>
+					<div className="relative w-fit">
+						<input className="bg-slate-200 input-display-msg" type="text" name="senderName" id="senderName" defaultValue={message.sender} disabled={true} />
+						<Icon icon="mage:user-fill" color="#0e63d6" className="icon-base top-[0.1rem] text-gray-500 text-xl text-opacity-40" />
+					</div>
 
-            <div className="w-full h-fit flex flex-col items-center gap-3 border-b-2 border-slate-400/20 pb-3">
-                <h2 className="text-2xl">Details:</h2>
-                <form className='w-full h-fit flex flex-col items-center gap-3'>
-                    <label className="w-full h-fit flex justify-center" htmlFor="senderName">From:</label>
-                    <div className='relative w-fit'>
-                        <input
-                            className="bg-slate-200 input-display-msg" type="text"
-                            name="senderName"
-                            id="senderName"
-                            defaultValue={message.sender}
-                            disabled={true}
-                        />
-                        <Icon
-                            icon='mage:user-fill'
-                            color='#0e63d6'
-                            className="icon-base top-[0.1rem] text-gray-500 text-xl text-opacity-40"
-                        />
-                    </div>
+					<label className="w-full h-fit flex justify-center" htmlFor="recipientName">
+						For:
+					</label>
+					<div className="relative w-fit">
+						<input className="bg-slate-200 input-display-msg" type="text" name="recipientName" id="recipientName" defaultValue={message.recipient} disabled={true} />
+						<Icon icon="mage:user-fill" color="#168709" className="icon-base top-[0.1rem] text-gray-500 text-xl" />
+					</div>
 
-                    <label className="w-full h-fit flex justify-center" htmlFor="recipientName">For:</label>
-                    <div className='relative w-fit'>
-                        <input
-                            className="bg-slate-200 input-display-msg" type="text"
-                            name="recipientName"
-                            id="recipientName"
-                            defaultValue={message.recipient}
-                            disabled={true}
-                        />
-                        <Icon
-                            icon='mage:user-fill'
-                            color='#168709'
-                            className="icon-base top-[0.1rem] text-gray-500 text-xl"
-                        />
-                    </div>
-
-                    <label className="w-full h-fit flex justify-center" htmlFor="receivedMessage">Message:</label>
-                    <textarea
-                        className="bg-slate-200 w-fit h-fit resize-none input-display-msg"
-                        type="text"
-                        name="receivedMessage"
-                        rows={4}
-                        id="receivedMessage"
-                        defaultValue={message.message}
-                        disabled={true}
-                    />
-                </form>
-
-            </div>
-            <div className='mt-5 pb-5'>
-                {user.userName !== message.sender && <SendMessageForm reply={true} recipientName={message.sender} />}
-            </div>
-        </Modal>
-    )
-
+					<label className="w-full h-fit flex justify-center" htmlFor="receivedMessage">
+						Message:
+					</label>
+					<textarea className="bg-slate-200 w-8/12 h-fit resize-none input-display-msg" type="text" name="receivedMessage" rows={4} id="receivedMessage" defaultValue={message.message} disabled={true} />
+				</form>
+			</div>
+			<div className="w-full flex justify-center mt-5 pb-5">{user.userName !== message.sender && <SendMessageForm reply={true} recipientName={message.sender} />}</div>
+		</Modal>
+	);
 }
 
 export function ReplyMessageModal({ isOpen, onRequestClose, message }) {
+	const recipientName = (isOpen && message && message.sender) || null;
 
-    const recipientName = isOpen && message && message.sender || null;
-
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            className='bg-slate-200 rounded-lg p-6 w-1/3 mx-auto mt-20 shadow-lg border-4 border-slate-400'
-            overlayClassName='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
-        >
-            <button onClick={onRequestClose}
-                className='relative left-[28.7rem] bottom-6 text-black hover:text-gray-600'
-            >X</button>
-            <SendMessageForm reply={true} recipientName={recipientName} />
-        </Modal>
-    )
+	return (
+		<Modal isOpen={isOpen} onRequestClose={onRequestClose} className="bg-slate-200  rounded-lg p-6 w-1/3 mx-auto mt-20 shadow-lg border-4 border-slate-400" overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+			<button onClick={onRequestClose} className="relative left-[28.7rem] bottom-6 text-black hover:text-gray-600">
+				X
+			</button>
+			<div className="w-full flex justify-center">
+				<SendMessageForm reply={true} recipientName={recipientName} />
+			</div>
+		</Modal>
+	);
 }
