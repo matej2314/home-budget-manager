d341d2ba01cc94aae540ba9a03ca08df5b463983
matej2314 <mateo2314@gmail.com>
2025-03-12 08:54:22 +0100

    .

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.options('*', (req, res) => {
-	res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+	res.header('Access-Control-Allow-Origin', req.get('Origin'));
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	res.header('Access-Control-Allow-Credentials', 'true');
