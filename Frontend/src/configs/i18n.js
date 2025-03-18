16428496814e5603b905871d62855d42a40febbd
matej2314 <mateo2314@gmail.com>
2025-03-18 09:15:09 +0100

    .

--- Frontend/src/configs/i18n.js
+++ Frontend/src/configs/i18n.js
@@ -1,42 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-const resources = {
-    en: {
-        common: require('../public/locales/en/common.json'),
-        aboutUs: require('../public/locales/en/aboutUs.json'),
-        dashboardComponents: require('../public/locales/en/dashboardComponents.json'),
-        dashboardInternal: require('../public/locales/en/dashboardInternal.json'),
-        errorPage: require('../public/locales/en/errorPage.json'),
-        forms: require('../public/locales/en/forms.json'),
-        homePage: require('../public/locales/en/homePage.json'),
-    },
-    pl: {
-        common: require('../public/locales/pl/common.json'),
-        aboutUs: require('../public/locales/pl/aboutUs.json'),
-        dashboardComponents: require('../public/locales/pl/dashboardComponents.json'),
-        dashboardInternal: require('../public/locales/pl/dashboardInternal.json'),
-        errorPage: require('../public/locales/pl/errorPage.json'),
-        forms: require('../public/locales/pl/forms.json'),
-        homePage: require('../public/locales/pl/homePage.json'),
-    }
-};
+import HttpApi from 'i18next-http-backend';
 
 i18n
+    .use(HttpApi)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        resources,
         fallbackLng: 'en',
         debug: false,
         interpolation: { escapeValue: false },
+
         ns: ['common', 'aboutUs', 'dashboardComponents', 'dashboardInternal', 'errorPage', 'forms', 'homePage'],
         defaultNS: 'common',
+
         detection: {
             order: ['navigator', 'cookie', 'localStorage'],
             caches: ['cookie', 'localStorage'],
         },
+
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+
         preload: ['en', 'pl'],
     });
 
