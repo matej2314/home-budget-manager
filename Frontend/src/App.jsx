84c6dd5fcfcf62a1a08322c8e89fd337fa0d0b5c
matej2314 <mateo2314@gmail.com>
2025-03-18 11:42:04 +0100

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,20 +7,20 @@ import './configs/i18n';
 export default function App() {
 
   const router = createBrowserRouter(routes, {
-    future: {
-      v7_startTransition: true,
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    }
+    // future: {
+    //   v7_startTransition: true,
+    //   v7_relativeSplatPath: true,
+    //   v7_fetcherPersist: true,
+    //   v7_normalizeFormMethod: true,
+    //   v7_partialHydration: true,
+    //   v7_skipActionErrorRevalidation: true,
+    // }
   })
 
   return (
     <>
-      <BackToUsMessage />
       <RouterProvider router={router} />
+      <BackToUsMessage />
       <ToastContainer
         position='top-center'
         autoClose={2000}
