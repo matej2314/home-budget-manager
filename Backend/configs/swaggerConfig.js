9a86a7b3c937c5be6df6b8ffa198504f5aa1c8fa
matej2314 <mateo2314@gmail.com>
2025-03-19 12:25:37 +0100

    .

--- Backend/configs/swaggerConfig.js
+++ Backend/configs/swaggerConfig.js
@@ -1,7 +1,7 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 const helmet = require('helmet');
+const path = require('path');
 
 const swaggerOptions = {
   definition: {
@@ -27,38 +27,8 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 const swaggerDocs = (app) => {
-
-  app.use('/api-docs', helmet.frameguard({ action: 'deny' }));
-  app.use('/api-docs', helmet.noSniff());
-  app.use('/api-docs', helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://budgetapp.msliwowski.net"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "https://budgetapp.msliwowski.net"],
-      connectSrc: ["'self'", "https://budgetapi.msliwowski.net", "wss://budgetapi.msliwowski.net"],
-      frameSrc: ["'self'"],
-      objectSrc: ["'none'"],
-      upgradeInsecureRequests: [],
-    },
-  }));
-
-  app.use('/api-docs', helmet.permissionPolicy({
-    features: {
-      fullscreen: ['*'],
-      geolocation: ['self'],
-      microphone: ['none'],
-      camera: ['none'],
-    },
-  }));
-
-  app.use('/api-docs', (req, res, next) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    next();
-  });
-
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 };
 
 module.exports = swaggerDocs;
