a69b8a23f2bb206265d9fb0bc401de15339bb25a
matej2314 <mateo2314@gmail.com>
2025-03-19 13:37:45 +0100

    changes in swaggerConfig to allow connection from jenkins

--- Backend/configs/swaggerConfig.js
+++ Backend/configs/swaggerConfig.js
@@ -29,6 +29,7 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 const swaggerDocs = (app) => {
   app.use('/api-docs', (req, res, next) => {
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://budgetapp.msliwowski.net; connect-src 'self' https://budgetapi.msliwowski.net wss://budgetapi.msliwowski.net; frame-src 'none'; object-src 'none'");
